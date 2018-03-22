8442a48db052ff1ac9461c2581048116ccead246
Mitch Stehula <mitch.stehula@gentex.com>
2018-03-22 10:10:58 -0400

    Added directory for genetic algorithms

---
+++ GeneticAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NeuralNetwork;
+
+namespace GeneticAlgorithms
+{
+    public class GeneticAlgorithm
+    {
+        public NeuralNetwork.NeuralNetwork[ ] Population { get; set; }
+
+    }
+}
